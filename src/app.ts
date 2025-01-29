1c4aef88a41db2f5b81dc9774c91244e2d5343c6
ivojr <stackcodeoficial@gmail.com>
2025-01-29 20:29:37 -0300

    servidor estatico foi criado

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import fastify from 'fastify';
 import { routes } from './routes';
 import fastifyMultipart from '@fastify/multipart';
 import cors from '@fastify/cors'
+import fastifyStatic from '@fastify/static';
+import path from 'path'
 
 const app = fastify({ logger: true })
 
 app.register(fastifyMultipart);
 app.register(routes);
-app.register(cors)
+app.register(cors);
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, '..', 'uploads'),
+  prefix: '/uploads'
+})
 
 export default app;
